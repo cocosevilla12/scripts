086738b9a7ae50cca6a6b78be024e1694ff24be7
cocosevilla12 <mbormar3004@g.educaand.es>
2024-10-25 15:49:57 +0200

    Update xhub.lua

--- xhub.lua
+++ xhub.lua
@@ -1,2 +1,3 @@
 --this is not mine.
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vexroxd/My-Script-/refs/heads/main/MM2-Candy-Farm"))()
